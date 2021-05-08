d456186e731933362c670239c60b92ed9703b248
deveezy <deveezysl@gmail.com>
2021-05-09 00:45:37 +0300

    signals mid

---
+++ src/ch10/10.7.c
@@ -0,0 +1,24 @@
+// #include	<signal.h>
+// #include  <stdio.h>
+// #include	<unistd.h>
+
+// static void
+// sig_alrm(int signo)
+// {
+// 	/* nothing to do, just return to wake up the pause */
+// }
+
+// unsigned int
+// sleep1(unsigned int seconds)
+// {
+// 	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+// 		return (seconds);
+// 	alarm(seconds);		/* start the timer */
+// 	pause();			/* next caught signal wakes us up */
+// 	return (alarm(0));	/* turn off timer, return unslept time */
+// }
+
+// int main(int argc, char** argv)
+// {
+//   return sleep1(1);
+// }
---
+++ src/ch10/jumps2.c
@@ -0,0 +1,30 @@
+// #include <setjmp.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int a(char *s, jmp_buf env)
+// {
+//   int i = setjmp(env);
+//   printf("Setjmp returned -- i = %d, %p\n", i, s);
+  
+//   printf("s = %s\n", s);
+//   return i;
+// }
+
+// int b(int i, jmp_buf env)
+// {
+//   printf("In B: i=%d.  Calling longjmp(env, i)\n", i);
+
+//   longjmp(env, i);
+// }
+
+// int main(int argc, char **argv)
+// {
+//   jmp_buf env;
+
+//   if (a("Jim", env) != 0) 
+//     exit(0);
+//   printf("shit\n");
+//   b(3, env);
+//   return 0;
+// }
